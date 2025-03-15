8179ef03980a3e040a8d1db7758d3c1d2681d4e8
shant0786 <rm.shanto786@gmail.com>
2025-03-16 03:05:35 +0600

    client setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
